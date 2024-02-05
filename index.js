7c765bc165b1b9387aaf9bd914c8818f99653218
Sedarkstian <juan99sebastianmelo@hotmail.com>
2024-02-04 23:14:30 -0500

    add logic indexjs

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ const session = require('express-session');
 const { Issuer, Strategy } = require('openid-client');
 const http = require("http");
 require("dotenv").config();
+//  2. Create an express application and set up session
+const app = express();
+app.use(session({
+    secret: 'my-secret',
+    resave: false ,
+    saveUninitialized: false
+}));
+
+// 3. Define application end-points
+app.get("/", (req, res) => {
+    res.send(" Lets build a Unified Digital Identity <br/><br/><a href='/login'> Affinidi Login</a>");
+})
+
+app.get('/login',
+    function (req, res, next) {
+        next();
+    },
+);
+
+app.get('/login/callback', (req, res, next) => {})
+
+app.get("/protected", (req, res) => {
+    res.header("Content-Type", 'application/json');
+    res.end("Protected Content for Authenticated Users");
+})
+
+// 4. Start the http server
+const httpServer = http.createServer(app)
+httpServer.listen(8080, () => {
+    console.log(`Hello World - Affinidi Login : Up and running on 8080`)
+})
